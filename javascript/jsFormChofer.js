4b3c16bae21d47cea12f9c316fcb2448f5e87f98
joyce <jebenite@espol.edu.ec>
2016-08-09 01:50:10 -0500

    creo q ya esta el form d la ruta

--- javascript/jsFormChofer.js
+++ javascript/jsFormChofer.js
@@ -1,6 +1,17 @@
 $( document ).ready(function(){
-  if( $('input.custom-control-input').prop('checked') ) {
-    alert('Seleccionado');
-}
 
+  $("input#checkIngreso.custom-control-input").on( 'change', function() {
+      if( $(this).is(':checked') ) {
+        $('input#horaIngreso.form-control').attr('required', true)
+      } else {
+        $('input#horaIngreso.form-control').removeAttr('required')
+      }
+  });
+  $("input#checkSalida.custom-control-input").on( 'change', function() {
+      if( $(this).is(':checked') ) {
+        $('input#horaSalida.form-control').attr('required', true)
+      } else {
+        $('input#horaSalida.form-control').removeAttr('required')
+      }
+  });
 })
